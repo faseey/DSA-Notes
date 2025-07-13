8c1f56d97918f8cc33c1fface0281483e09e6f3d
Fasih Khalil <www.fasiih@gmail.com>
2025-07-13 16:52:53 +0500

    leetcode 1

--- src/Arrays/TwoSum/twoSum.java
+++ src/Arrays/TwoSum/twoSum.java
@@ -5,6 +5,18 @@ import java.util.Map;
 
 public class twoSum {
 
+    public static int[] brutetwoSum(int[] nums, int target) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n-1;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null; // No solution found
+    }//o(N^2)
+
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
@@ -23,8 +35,13 @@ public class twoSum {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] result = twoSum(nums, target);
+        int[] bruteresult = brutetwoSum(nums, target);
 
         if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }if (bruteresult != null) {
             System.out.println("Indices: " + result[0] + ", " + result[1]);
         } else {
             System.out.println("No solution found.");
