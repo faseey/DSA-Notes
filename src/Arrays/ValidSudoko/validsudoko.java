b970e03233c95d5d856e097f60b0918394f6348c
Fasih Khalil <www.fasiih@gmail.com>
2025-07-13 16:56:40 +0500

    leetcode 36

--- src/Arrays/ValidSudoko/validsudoko.java
+++ src/Arrays/ValidSudoko/validsudoko.java
@@ -30,41 +30,73 @@ public class validsudoko {
     }
 
     //  Brute Force Version – Time: O(9^3) = O(729)  O(N^3)
-    public static boolean isValidSudokuBrute(char[][] board) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                char num = board[i][j];
-                if(num == '.') continue;
-
-                // check row
-                for(int k = 0; k < 9; k++) {
-                    if(k != j && board[i][k] == num) return false;
-                }
-
-                // check column
-                for(int k = 0; k < 9; k++) {
-                    if(k != i && board[k][j] == num) return false;
-                }
-
-                // check 3x3 box
-                int boxRow = (i / 3) * 3;
-                int boxCol = (j / 3) * 3;
-                for(int r = boxRow; r < boxRow + 3; r++) {
-                    for(int c = boxCol; c < boxCol + 3; c++) {
-                        if(r != i && c != j && board[r][c] == num) return false;
-                    }
+    public static boolean traverse(char[][] board,int sr,int er ,int sc,int ec){
+        Set<Character> set = new HashSet<>();
+        for(int row = sr;row<=er;row++){
+            for(int col = sc;col<=ec;col++){
+                if(board[row][col] == '.')
+                    continue;
+                else if(set.contains(board[row][col]))
+                    return false;
+                else{
+                    set.add(board[row][col]);
                 }
             }
         }
         return true;
     }
+    public static boolean isValidSudokuBrute(char[][] board) {
+
+        for(int row=0;row<9;row++){
+            Set<Character> set = new HashSet<>();
+            for(int col=0;col<9;col++){
+                if(board[row][col] == '.')
+                    continue;
+                else if(set.contains(board[row][col]))
+                    return false;
+                else{
+                    set.add(board[row][col]);
+                }
+            }
+        }
+        for(int col=0;col<9;col++){
+            Set<Character> set = new HashSet<>();
+            for(int row=0;row<9;row++){
+                if(board[row][col] == '.')
+                    continue;
+                else if(set.contains(board[row][col]))
+                    return false;
+                else{
+                    set.add(board[row][col]);
+                }
+            }
+        }
+
+        boolean flag = false;
+        for(int sr=0;sr<9; sr+=3){
+            int er = sr+2;
+
+            for(int sc =0;sc<9;sc+=3){
+                int ec = sc+2;
+
+                flag =traverse(board,sr,er,sc,ec);
+                if(!flag){
+                    return false;
+                }
+            }
+
+        }
+        return flag;
+
+    }
+
 
     // 🧪 Test Main Method
     public static void main(String[] args) {
 
 
         char[][] board = {
-                {'8','3','.','.','7','.','.','.','.'},
+                {'5','3','.','.','7','.','.','.','.'},
                 {'6','.','.','1','9','5','.','.','.'},
                 {'.','9','8','.','.','.','.','6','.'},
                 {'8','.','.','.','6','.','.','.','3'},
