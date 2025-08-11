a757477cfaa20311aa5a43e9235448cb4e178397
Fasih Khalil <www.fasiih@gmail.com>
2025-08-11 06:28:10 +0500

    stack implementation

---
+++ src/Stacks/Implementation/StackException.java
@@ -0,0 +1,8 @@
+package Stacks.Implementation;
+
+public class StackException extends Exception{
+
+    public StackException(String message) {
+        super(message);
+    }
+}
---
+++ src/Stacks/Implementation/Stackmain.java
@@ -0,0 +1,23 @@
+package Stacks.Implementation;
+
+
+public class Stackmain {
+    public static void main(String[] args) throws StackException {
+        builtstack stack = new DynamicStack(5);
+
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
+        stack.push(9);
+        stack.push(18);
+        stack.push(89);
+
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
