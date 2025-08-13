6355504d5f2b067a7316643ad5d1a3a6f3bcb72e
Fasih Khalil <www.fasiih@gmail.com>
2025-08-14 01:18:03 +0500

    trees

---
+++ src/Trees/Questions/levelordertraversal.java
@@ -0,0 +1,34 @@
+package Trees.Questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class levelordertraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+
+            for(int i =0;i<size;i++){
+                TreeNode temp = q.poll();
+                list.add(temp.val);
+
+                if(temp.left != null) q.offer(temp.left);
+                if(temp.right != null) q.offer(temp.right);
+            }
+            res.add(list);
+
+        }
+        return res;
+
+    }
+}
