c0c98acd375995d7fbe7981e089b1fe067b1df97
Fasih Khalil <www.fasiih@gmail.com>
2025-08-25 08:33:27 +0500

    tree questions

---
+++ src/Trees/Questions/BFS/symmetric_tree.java
@@ -0,0 +1,37 @@
+package Trees.Questions.BFS;
+
+import Trees.Questions.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class symmetric_tree  {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root.left);
+        q.offer(root.right);
+
+        while(!q.isEmpty()){
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+
+
+            if(left == null && right == null){
+                continue;
+            }
+            if(left == null || right == null){
+                return false;
+            }
+            if(left.val != right.val){
+                return false;
+            }
+            q.offer(left.left);
+            q.offer(right.right);
+            q.offer(left.right);
+            q.offer(right.left);
+        }
+        return true;
+
+    }
+}
