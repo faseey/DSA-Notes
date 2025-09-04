9bdc4bbb1ea9634a0596df6a1e025f89816657b0
Fasih Khalil <www.fasiih@gmail.com>
2025-09-04 10:18:41 +0500

    Trees

---
+++ src/Trees/Questions/BFS/invert_binarytree.java
@@ -0,0 +1,17 @@
+package Trees.Questions.BFS;
+
+import Trees.Questions.TreeNode;
+
+public class invert_binarytree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode left  = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
